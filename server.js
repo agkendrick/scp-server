4493680ebc91ba91c0a4c23bb96e4a0568828184
Aaron <agkendrick1@gmail.com>
2020-08-18 18:43:07 -0500

    Add CORS

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const getAverages = require('./index').getAverages;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', async (req, res) => {
     try {
